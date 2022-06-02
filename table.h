d639b1b70cd1b622a4343523e7844f2a97f960c5
d-egorov02 <89845764+d-egorov02@users.noreply.github.com>
2022-06-02 20:51:51 +0700

    Update table.h

--- table.h
+++ table.h
@@ -1,3 +1,4 @@
+// задание таблицы с произвольным типом данных T
 #include <vector>
 #include <algorithm>
 
@@ -6,20 +7,20 @@ class Table {
     size_t numRows, numCols;
     std::vector< std::vector<T> > v;
     public:
-        Table(size_t row, size_t col) : numRows(row), numCols(col){ v = std::vector< std::vector<T> >(row,  std::vector<T>(col)); };
-        std::vector<T> operator[](const size_t i) const{
+        Table(size_t row, size_t col) : numRows(row), numCols(col){ v = std::vector< std::vector<T> >(row,  std::vector<T>(col)); }; // конструктор, задание размера
+        std::vector<T> operator[](const size_t i) const{ // реализация оператора [] (в итоге возможно обращение [][])
             return v[i];
         }
         std::vector<T>& operator[](const size_t i){
             return v[i];
         }
-        void Resize(size_t newN, size_t newM){
+        void Resize(size_t newN, size_t newM){ // изменение размера
             numRows = newN; numCols = newM;
             v.resize(numRows);
             for(auto& it : v)
                 it.resize(numCols);
         }
-        std::pair<size_t, size_t> Size() const{
+        std::pair<size_t, size_t> Size() const{ // возвращение размера
             return {numRows, numCols};
         }
 };
